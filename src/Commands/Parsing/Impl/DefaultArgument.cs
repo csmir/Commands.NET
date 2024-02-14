e985a714394ccafa0fbcbac31bc490e3c453f13b
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2024-02-14 12:02:01 +0100

    Implement parsing argument differenciation

---
+++ src/Commands/Parsing/Impl/DefaultArgument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.Parsing
+{
+    public readonly struct DefaultArgument(string? value) : IParseArgument
+    {
+        public object? Value { get; } = value;
+
+        public static implicit operator DefaultArgument(string? value)
+        {
+            return new DefaultArgument(value);
+        }
+    }
+}
---
+++ src/Commands/Parsing/Impl/KeyedArgument.cs
@@ -0,0 +1,14 @@
+﻿namespace Commands.Parsing
+{
+    public readonly struct KeyedArgument(string key, string? value) : IParseArgument
+    {
+        public object? Value { get; } = value;
+
+        public string Key { get; } = key;
+
+        public static implicit operator KeyedArgument((string key, string? value) pair)
+        {
+            return new KeyedArgument(pair.key, pair.value);
+        }
+    }
+}
