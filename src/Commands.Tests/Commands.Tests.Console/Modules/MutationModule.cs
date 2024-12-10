21c0ba2b170ed97eb510ada1fe0d2f90f6b0fbec
Armano den Boef <csmir@outlook.com>
2024-12-10 17:01:05 +0100

    Example module for modifying module commands

---
+++ src/Commands.Tests/Commands.Tests.Console/Modules/MutationModule.cs
@@ -0,0 +1,16 @@
+﻿namespace Commands.Tests
+{
+    public class MutationModule : ModuleBase
+    {
+        [Name("add-command")]
+        public async Task MutateCurrentModule(string commandName)
+        {
+            var command = new CommandBuilder();
+
+            command.WithAliases(commandName);
+            command.WithDelegate(() => "Hello, World!");
+
+            Command.Module!.Components.Add(command.Build(Manager.Configuration));
+        }
+    }
+}
