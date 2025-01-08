1c75eeb012e5661424b7c59d24285ee7a263d1d2
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2025-01-08 20:14:37 +0100

    Logic adaptations

--- src/Commands/Testing/TestUtilities.cs
+++ src/Commands/Testing/TestUtilities.cs
@@ -1,9 +1,32 @@
-﻿namespace Commands.Testing;
+﻿using Commands.Parsing;
+
+namespace Commands.Testing;
 
 internal static class TestUtilities
 {
     internal static async ValueTask<TestResult> Test(this Command command, ICallerContext caller, ITestProvider provider, CommandOptions options)
     {
+        TestResult GetResult(IExecuteResult result)
+        {
+            TestResult CompareReturn(TestResultType targetType, Exception exception)
+            {
+                return provider.ShouldEvaluateTo == targetType
+                    ? TestResult.FromSuccess(command, provider.ShouldEvaluateTo)
+                    : TestResult.FromError(command, provider.ShouldEvaluateTo, targetType, exception);
+            }
+
+            return result.Exception switch
+            {
+                null => CompareReturn(TestResultType.Success, new InvalidOperationException("The command was expected to fail, but it succeeded.")),
+
+                CommandParsingException    => CompareReturn(TestResultType.ParseFailure, result.Exception),
+                CommandEvaluationException => CompareReturn(TestResultType.ConditionFailure, result.Exception),
+                CommandOutOfRangeException => CompareReturn(TestResultType.MatchFailure, result.Exception),
+
+                _ => CompareReturn(TestResultType.InvocationFailure, result.Exception),
+            };
+        }
+
         // Create a new set of arguments by appending the arguments from the provider to the command's name. This should result in valid parameters for an ArgumentArray.
         var commandName = command.GetFullName(false)
             .Split([' '], StringSplitOptions.RemoveEmptyEntries)
@@ -26,32 +49,11 @@ internal static class TestUtilities
             if (parseResult[i].Success)
                 argumentObjects[i] = parseResult[i].Value;
             else
-                return provider.GetResult(command, parseResult[i]);
+                return GetResult(ParseResult.FromError(new CommandParsingException(command, parseResult[i].Exception)));
         }
 
         var runResult = await command.Run(caller, argumentObjects, options);
 
-        return provider.GetResult(command, runResult);
-    }
-
-    internal static TestResult GetResult(this ITestProvider provider, Command command, IExecuteResult result)
-    {
-        TestResult CompareReturn(TestResultType targetType, Exception exception)
-        {
-            return provider.ShouldEvaluateTo == targetType
-                ? TestResult.FromSuccess(command, provider.ShouldEvaluateTo)
-                : TestResult.FromError(command, provider.ShouldEvaluateTo, targetType, exception);
-        }
-
-        return result.Exception switch
-        {
-            null => CompareReturn(TestResultType.Success, new InvalidOperationException("The command was expected to fail, but it succeeded.")),
-
-            ParserException            => CompareReturn(TestResultType.ParseFailure, result.Exception),
-            CommandEvaluationException => CompareReturn(TestResultType.ConditionFailure, result.Exception),
-            CommandOutOfRangeException => CompareReturn(TestResultType.MatchFailure, result.Exception),
-
-            _ => CompareReturn(TestResultType.InvocationFailure, result.Exception),
-        };
+        return GetResult(runResult);
     }
 }
