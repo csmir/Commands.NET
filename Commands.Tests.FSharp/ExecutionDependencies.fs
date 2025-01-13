d61d97095ce86f98e41ea249f7d13fea031883fa
Armano den Boef <csmir@outlook.com>
2025-01-13 16:26:06 +0100

    Add FSharp sample project

---
+++ Commands.Tests.FSharp/ExecutionDependencies.fs
@@ -0,0 +1,7 @@
+﻿module ExecutionDependencies
+
+open Commands
+
+type public LocalCallerContext() = 
+    interface ICallerContext with 
+        member this.Respond (message: obj): unit = printfn "%s" (message.ToString());
---
+++ Commands.Tests.FSharp/Program.fs
@@ -0,0 +1,16 @@
+﻿open Commands;
+open System;
+
+open ExecutionDependencies;
+
+let handler = new DelegateResultHandler<ICallerContext>(
+    Action<ICallerContext, IExecuteResult, IServiceProvider>(fun (context: ICallerContext) (result: IExecuteResult) (services: IServiceProvider) -> printfn "%A" result))
+
+let command = Command.Create(Func<string>(fun () -> "Hello world"), [| "hello" |])
+
+let handlers = [| handler :> ResultHandler |]
+let commands = [| command :> IComponent |]
+
+let manager = ComponentManager.Create(commands, handlers)
+
+manager.TryExecute<LocalCallerContext>(new LocalCallerContext(), "hello")
