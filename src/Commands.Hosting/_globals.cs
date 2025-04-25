87edd6faca473db52965f28a1efdb4b4bbbd50b3
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2025-04-25 20:32:20 +0200

    Move global usings to _global

--- src/Commands.Hosting/_globals.cs
+++ src/Commands.Hosting/_globals.cs
@@ -2,4 +2,7 @@
  * This file contains global usings that are available to all files in the project.
  */
 
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
 global using System.Diagnostics.CodeAnalysis;
